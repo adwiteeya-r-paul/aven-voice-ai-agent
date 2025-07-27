8a3711e26a31ee3ae6ade97bc321e681ee83e865
Adwiteeya R Paul <153979848+adwiteeya-r-paul@users.noreply.github.com>
2025-07-28 03:48:04 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,19 +11,35 @@ export default function Home() {
       </main>
 
       <vapi-widget
-        assistant-id="05fc5585-41b3-479e-a6c4-979a824aec23"
         public-key="5aa52943-5448-433d-999f-0dc4e5b7ab5a"
+        assistant-id="05fc5585-41b3-479e-a6c4-979a824aec23"
+        mode="voice"
+        theme="dark"
+        base-bg-color="#000000"
+        accent-color="#14B8A6"
+        cta-button-color="#000000"
+        cta-button-text-color="#ffffff"
+        border-radius="large"
+        size="full"
+        position="bottom-right"
+        title="TALK WITH AI"
+        start-button-text="Start"
+        end-button-text="End Call"
+        chat-first-message="Hey, How can I help you today?"
+        chat-placeholder="Type your message..."
+        voice-show-transcript="true"
+        consent-required="true"
+        consent-title="Terms and conditions"
+        consent-content="By clicking \"Agree,\" and each time I interact with this AI agent, I consent to the recording, storage, and sharing of my communications with third-party service providers, and as otherwise described in our Terms of Service."
+        consent-storage-key="vapi_widget_consent"
       ></vapi-widget>
 
       <Script
         src="https://unpkg.com/@vapi-ai/client-sdk-react/dist/embed/widget.umd.js"
         async
         strategy="lazyOnload"
-        onLoad={() => {
-          console.log("Vapi SDK Script loaded successfully from unpkg.");
-        }}
-        onError={(e) => console.error("Error loading Vapi SDK script from unpkg:", e)}
       />
     </div>
   );
 }
+
