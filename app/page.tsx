5319ef31414c0ddccc24f0319bcd940beef34a1b
Adwiteeya R Paul <153979848+adwiteeya-r-paul@users.noreply.github.com>
2025-07-28 01:11:51 +0600

    final page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,30 @@
-//app/page.js
+// app/page.js
 
-import Head from 'next/head';
-                
-import Script from 'next/script'; // Import Script component
+import Script from 'next/script'; 
+
+export const metadata = {
+  title: 'Abby is here to support you!',
+};
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Abby is here to support you!</title>
-        {/* Other meta tags */}
-      </Head>
-
+     
       <main>
         <h1>Welcome to Aven Customer Support!</h1>
         <p>Click the Vapi widget below to start a conversation with Abby.</p>
-        {/* Your other Next.js content */}
       </main>
 
-      {/* VAPI WIDGET SCRIPT */}
+      {/* VAPI WIDGET SCRIPTS */}
       <Script src="https://vapi.ai/widgets/v1/script.js" strategy="lazyOnload" />
       <Script strategy="lazyOnload" id="vapi-init-script">
         {`
           Vapi.listen({
-              assistantId: "05fc5585-41b3-479e-a6c4-979a824aec23", 
+              assistantId: "05fc5585-41b3-479e-a6c4-979a824aec23",
               button: true
           });
         `}
       </Script>
-      }
-
     </div>
   );
 }
